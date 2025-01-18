b8c059c268ef7eba70440e2f0a70cbc0e2622c89
TolMen <cotton.frachisse@gmail.com>
2025-01-18 15:55:29 +0100

    Premier jets des tables et de la bdd

---
+++ bdd.sql
@@ -0,0 +1,46 @@
+-- Configuration BDD
+SET NAMES utf8mb4;
+CREATE DATABASE IF NOT EXISTS siop1_wiki;
+USE siop1_wiki;
+
+
+-- Création des tables :
+-- Table `users`
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE, -- UNIQUE pour éviter les doublons
+    password VARCHAR(255) NOT NULL,
+    role ENUM('user', 'admin') NOT NULL -- ENUM pour obliger à choisir entre 'user' ou 'admin'
+) ENGINE=InnoDB;
+
+-- Table `articles`
+CREATE TABLE articles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Insére la date et l'heure au moment de la création
+    updated_at DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP, -- Insére la date et l'heure au moment de la mise à jour
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+-- Table `article_versions`
+CREATE TABLE article_versions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    updated_by INT NOT NULL,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    article_id INT NOT NULL, -- Insére la date et l'heure au moment de la création
+    FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE,
+    FOREIGN KEY (updated_by) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+-- Table `bans`
+CREATE TABLE bans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    reason TEXT NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME DEFAULT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+) ENGINE=InnoDB;
