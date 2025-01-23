2b2995a6bda66a5335e66eae721f27d0780991f8
TolMen <cotton.frachisse@gmail.com>
2025-01-23 11:22:22 +0100

    Création d'un composant pour les balises meta répétitive

---
+++ src/component/head.php
@@ -0,0 +1,22 @@
+    <!-- Balises méta essentielles -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="mobile-web-app-capable" content="yes" />
+
+    <!-- Informations SEO -->
+    <meta name="keywords" content="article, wiki, civilisations, histoire, cultures anciennes, connaissances, partage, gestion de contenu, SLAM, BTS SIO, projet collaboratif, PHP, développement web">
+    <meta name="description" content="Explorez les civilisations et l'histoire grâce à une plateforme de gestion de contenu conçue en équipe, utilisant PHP, Bootstrap et des pratiques modernes de développement web.
+    Un projet SLAM réalisé dans le cadre du BTS SIO en équipe de 3." />
+    <meta name="author" content="Nolan / Kelly / Jessy" />
+
+    <!-- Icône du site -->
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon.ico" />
+
+    <!-- Feuilles de style externes -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
+
+    <!-- Feuilles de style personnalisées -->
+    <link rel="stylesheet" href="css/baseStyle.css" />
