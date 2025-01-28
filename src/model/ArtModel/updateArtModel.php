e528810c7e983b249f9cd55d3df75ce3a22c6abd
TolMen <cotton.frachisse@gmail.com>
2025-01-28 09:20:17 +0100

    Ajout model update article

---
+++ src/model/ArtModel/updateArtModel.php
@@ -0,0 +1,30 @@
+<?php
+
+/* 
+- Inclusion des fichiers nécessaire
+*/
+require_once '../../control/BDDControl/connectBDD.php';
+
+class UpdateArticleModel
+{
+
+    /*
+    - Cette fonction récupére les informations des articles
+    */
+    public function getArticleId(PDO $bdd, $articleId)
+    {
+        $recupArt = $bdd->prepare('SELECT * FROM articles WHERE id = ?');
+        $recupArt->execute([$articleId]);
+        return $recupArt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    /*
+    - Fonction pour mettre à jour les informations de l'article
+    */
+    public function updateArticle(PDO $bdd, $articleId, $title, $content, $updatedAt)
+    {
+        $updateArt = 'UPDATE articles SET title = ?, content = ?, updatedAt = ? WHERE id = ?';
+        $updateArticle = $bdd->prepare($updateArt);
+        return $updateArticle->execute([$title, $content, $updatedAt, $articleId]);
+    }
+}
