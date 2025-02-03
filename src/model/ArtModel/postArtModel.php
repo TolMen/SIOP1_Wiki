0e320161b73ad35ece9c7c78a02a9959b7cce3c6
TolMen <cotton.frachisse@gmail.com>
2025-02-03 12:28:14 +0100

    Ajout de la requete pour le user_id en username

--- src/model/ArtModel/postArtModel.php
+++ src/model/ArtModel/postArtModel.php
@@ -17,4 +17,16 @@ class ArtPostModel
         $recupPostArt->execute(['id' => $postArtId]);
         return $recupPostArt->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    /*
+    - Cette fonction récupère le pseudo de l'utilisateur qui a écrit l'article
+    */
+    public function getPostArtUser(PDO $bdd, $postArtUserId)
+    {
+        $recupPostArtUser = $bdd->prepare(
+            'SELECT username FROM users WHERE id = :id'
+        );
+        $recupPostArtUser->execute(['id' => $postArtUserId]);
+        return $recupPostArtUser->fetch(PDO::FETCH_ASSOC);
+    }
 }
