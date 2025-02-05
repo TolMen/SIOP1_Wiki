2e446202a83fa3d3fff86fd11328c93151e67c51
TolMen <cotton.frachisse@gmail.com>
2025-02-05 11:43:38 +0100

    Refonte design

--- src/control/ArtControl/postArt.php
+++ src/control/ArtControl/postArt.php
@@ -38,18 +38,35 @@ foreach ($articles as $article) {
 */
     $dateToShow = !empty($article['updatedAt']) ? $article['updatedAt'] : $article['createdAt'];
 ?>
-    <h2 class="title"><?= htmlspecialchars($article['title']); ?></h2>
-    <i class="separator"></i>
-    <div class="littleInfo">
-        <p class="author">Utilisateur : <?= htmlspecialchars($userArticles['username'] ?? 'Inconnu'); ?></p>
-        <p class="date"><?= date("d/m/Y", strtotime($dateToShow)); ?></p>
+    <div class="container mt-4">
+        <div class="row">
+            <!-- Colonne gauche : Article -->
+            <div class="col-md-8" style="padding-right: 100px;">
+                <h2 class="title font-weight-bold border-bottom pb-4 text-center"> <?= htmlspecialchars($article['title']); ?> </h2>
+
+                <div class="text text-center"> <?= nl2br($article['content']); ?> </div>
+
+                <!-- Signature en bas -->
+                <div class="text-muted border-top pt-1">
+                    <p class="mb-0 mt-2">Utilisateur : <?= htmlspecialchars($userArticles['username'] ?? 'Inconnu'); ?></p>
+                    <p class="mb-0">Publié le : <?= date("d/m/Y", strtotime($dateToShow)); ?></p>
+                </div>
+
+                <!-- Liens pour l'historique et la modification -->
+                <div class="mt-2">
+                    <a href="templateArtV.php?articleVID=<?php echo $postArtId; ?>" class="btn btn-outline-primary btn-sm">Historique</a>
+                    <a href="updateArt.php?articleID=<?php echo $postArtId; ?>" class="btn btn-outline-secondary btn-sm">Modification</a>
+                </div>
+            </div>
+
+            <!-- Colonne droite : Image par défaut qui suit le scroll et reste centrée -->
+            <div class="col-md-4 d-flex justify-content-center d-none d-md-block">
+                <div class="sticky-top" style="top: 175px; height: 450px;">
+                    <img src="assets/img/civilisation.jpg" alt="Image par défaut" class="img-fluid rounded shadow mx-auto d-block">
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="link">
-        <a href="templateArtV.php?articleVID=<?php echo $postArtId; ?>">Historique -</a>
-        <a href="updateArt.php?articleID=<?php echo $postArtId; ?>"> - Modification</a>
-    </div>
-    <i class="separator"></i>
-    <div class="text"><?= nl2br($article['content']); ?></div>
 <?php
 }
 ?>
