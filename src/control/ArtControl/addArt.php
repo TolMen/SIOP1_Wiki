dba01b6a6063a72916a5c78512b1863f5132eabc
TolMen <cotton.frachisse@gmail.com>
2025-02-06 09:42:33 +0100

    Implémentation logique ajout d'images et compression automatique à 75%

--- src/control/ArtControl/addArt.php
+++ src/control/ArtControl/addArt.php
@@ -3,17 +3,17 @@
 session_name("main");
 session_start();
 
-/* 
-- Inclusion des fichiers nécessaire
+/*
+- Inclusion des fichiers nécessaires
 */
 require_once '../../model/ArtModel/addArtModel.php';
+require_once '../../model/Services/ImageService.php'; // Inclure le service d'image
 
 if (isset($_POST['publishArticle'])) {
-
     /*
     - Sécurisation des données
     */
-    $title = $_POST['title'];
+    $title = htmlspecialchars($_POST['title']);
     $content = strip_tags($_POST['content'], '<p><b><i><u><strong><em><h1><h2><h3><h4><h5><h6><ol><ul><li><a><img><table><tr><td><th><tbody><thead><tfoot><caption><colgroup><col><pre><code><blockquote><q><hr><br><span><sup><sub><del><ins><mark><video><audio><source><iframe><address><time><article><aside><figcaption><figure><details><summary><kbd>');
     $createdAt = $_POST['createdAt'];
     $userID = $_SESSION['userID'];
@@ -22,7 +22,40 @@ if (isset($_POST['publishArticle'])) {
     - Crée une instance de classe, puis récupère les informations
     */
     $addArticleModel = new AddArticleModel();
-    if ($addArticleModel->insertArticle($bdd, $title, $content, $createdAt, $userID)) {
+    $articleID = $addArticleModel->insertArticle($bdd, $title, $content, $createdAt, $userID);
+
+    if ($articleID) {
+        // Vérifier si un fichier image est envoyé
+        if (isset($_FILES['images']) && $_FILES['images']['error'] == 0) {
+            $uploadDir = '../../../assets/imgUpload/'; // Dossier cible
+
+            $fileTmpPath = $_FILES['images']['tmp_name'];
+            $fileName = $_FILES['images']['name'];
+            $fileSize = $_FILES['images']['size'];
+            $fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+            // Vérifications des types autorisés
+            $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
+
+            if (!in_array($fileExt, $allowedTypes)) {
+                echo "Format non supporté.";
+                exit;
+            }
+
+            // Générer un nom unique avec l'ID de l'article
+            $uniqueName = 'imgArticle' . $articleID . '_' . uniqid() . '.' . $fileExt;
+            $destPath = $uploadDir . $uniqueName;
+
+            // Compression et redimensionnement (Max: 800x800, Qualité: 75)
+            if (ImageService::compressAndResizeImage($fileTmpPath, $destPath, 800, 800, 75)) {
+                // Insérer l’image en BDD
+                $imgUrl = 'assets/imgUpload/' . $uniqueName;
+                $addArticleModel->insertImage($bdd, $imgUrl, $createdAt, $articleID);
+            } else {
+                echo "Erreur lors de la compression de l'image.";
+                exit;
+            }
+        }
 
         /*
         - Redirection vers le tableau de bord
