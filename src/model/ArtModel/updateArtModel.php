116f3a98230145fe5ddbb3751706fbd143b99dec
TolMen <cotton.frachisse@gmail.com>
2025-02-06 11:00:42 +0100

    Implémentation requete pour update l'image ou non + Logique pour l'archive de l'article

--- src/model/ArtModel/updateArtModel.php
+++ src/model/ArtModel/updateArtModel.php
@@ -7,9 +7,8 @@ require_once '../../control/BDDControl/connectBDD.php';
 
 class UpdateArticleModel
 {
-
     /*
-    - Cette fonction récupére les informations des articles
+    - Cette fonction récupère les informations des articles
     */
     public function getArticleId(PDO $bdd, $articleId)
     {
@@ -21,11 +20,11 @@ class UpdateArticleModel
     /*
     - Fonction pour sauvegarder l'ancienne version de l'article
     */
-    public function saveArticleVersion(PDO $bdd, $articleId, $title, $content, $createdAt, $userId)
+    public function saveArticleVersion(PDO $bdd, $articleId, $title, $content, $createdAt, $userId, $imageUrl)
     {
-        $insertVersion = 'INSERT INTO article_versions (title, content, createdAt, user_id, article_id) VALUES (?, ?, ?, ?, ?)';
+        $insertVersion = 'INSERT INTO article_versions (title, content, createdAt, user_id, article_id, image_url) VALUES (?, ?, ?, ?, ?, ?)';
         $stmt = $bdd->prepare($insertVersion);
-        return $stmt->execute([$title, $content, $createdAt, $userId, $articleId]);
+        return $stmt->execute([$title, $content, $createdAt, $userId, $articleId, $imageUrl]);
     }
 
     /*
@@ -37,4 +36,25 @@ class UpdateArticleModel
         $updateArticle = $bdd->prepare($updateArt);
         return $updateArticle->execute([$title, $content, $userId, $articleId]);
     }
+
+    // - Fonction pour mettre à jour ou insérer l'image de l'article
+    public function updateImage(PDO $bdd, $articleId, $newImageUrl)
+    {
+        // Vérifier s'il existe déjà une image pour cet article
+        $checkImage = $bdd->prepare('SELECT id FROM images WHERE article_id = ?');
+        $checkImage->execute([$articleId]);
+
+        // Si une image existe, mettez à jour l'URL de l'image
+        if ($checkImage->rowCount() > 0) {
+            $updateImage = 'UPDATE images SET url = ?, createdAt = NOW() WHERE article_id = ?';
+            $stmt = $bdd->prepare($updateImage);
+            return $stmt->execute([$newImageUrl, $articleId]);
+        }
+
+        // Si aucune image n'existe, insérer une nouvelle entrée dans la table images
+        $insertImage = 'INSERT INTO images (url, createdAt, article_id) VALUES (?, NOW(), ?)';
+        $stmt = $bdd->prepare($insertImage);
+        return $stmt->execute([$newImageUrl, $articleId]);
+    }
+
 }
