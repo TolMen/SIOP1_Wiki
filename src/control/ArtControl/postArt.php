0842578abb78326a1884b14fdc7bb4faaceacbb8
TolMen <cotton.frachisse@gmail.com>
2025-02-13 14:41:10 +0100

    CSS image mobile

--- src/control/ArtControl/postArt.php
+++ src/control/ArtControl/postArt.php
@@ -43,32 +43,33 @@ foreach ($articles as $article) {
 ?>
     <div class="container mt-4">
         <div class="row">
-            <!-- Colonne gauche : Article -->
-            <div class="col-md-8" style="padding-right: 100px;">
+            <div class="col-md-8 article-container">
                 <h2 class="title font-weight-bold border-bottom pb-4 text-center"> <?= htmlspecialchars($article['title']); ?> </h2>
+
+                <!-- Image pour la vue mobile -->
+                <div class="text-center d-block d-md-none">
+                    <img src="<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'article" class="img-fluid rounded shadow mx-auto d-block mb-3" style="height: 225px; margin: 25px 0">
+                </div>
 
                 <div class="text text-center"> <?= nl2br($article['content']); ?> </div>
 
-                <!-- Signature en bas -->
                 <div class="text-muted border-top pt-1">
                     <p class="mb-0 mt-2">Utilisateur : <?= htmlspecialchars($userArticles['username'] ?? 'Inconnu'); ?></p>
                     <p class="mb-0">Publié le : <?= date("d/m/Y", strtotime($dateToShow)); ?></p>
                 </div>
 
-                <!-- Liens pour l'historique et la modification -->
                 <div class="mt-2">
                     <a href="templateArtV.php?articleVID=<?php echo $postArtId; ?>" class="btn btn-outline-primary btn-sm">Historique</a>
                     <a href="updateArt.php?articleID=<?php echo $postArtId; ?>" class="btn btn-outline-secondary btn-sm">Modification</a>
                 </div>
             </div>
 
-            <!-- Colonne droite : Image qui suit le scroll -->
+            <!-- Image pour la vue desktop -->
             <div class="col-md-4 d-flex justify-content-center d-none d-md-block">
                 <div class="sticky-top" style="top: 175px; height: 450px;">
                     <img src="<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'article" class="img-fluid rounded shadow mx-auto d-block">
                 </div>
             </div>
-
         </div>
     </div>
 <?php
