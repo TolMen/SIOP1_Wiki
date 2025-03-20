6865dc4cc37a8e23ae552fe1b07963fe6ab5f6d3
nolansio <nolanarsac105@gmail.com>
2025-03-20 14:45:05 +0100

    Ajout d'une modale pour afficher les messages avec détails et amélioration du style CSS pour la messagerie

--- messagerie.php
+++ messagerie.php
@@ -5,7 +5,7 @@ session_start();
 require_once 'src/control/BDDControl/connectBDD.php'; // Connexion à la BDD
 
 if (!empty($_SESSION["userID"]) && $_SESSION["userRole"] == "admin") {
-    $query = $bdd->prepare("SELECT name, email, subject, message FROM contact");
+    $query = $bdd->prepare("SELECT id, name, email, subject, message FROM contact");
     $query->execute(array());
     $messages = $query->fetchAll();
 } else {
@@ -21,6 +21,9 @@ if (!empty($_SESSION["userID"]) && $_SESSION["userRole"] == "admin") {
 
     <head>
         <?php include 'src/component/head.php'; ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
         <link rel="stylesheet" href="css/messagerie.css" />
 
         <title>Wiki - Messagerie</title>
@@ -31,17 +34,34 @@ if (!empty($_SESSION["userID"]) && $_SESSION["userRole"] == "admin") {
         <h1>Messagerie</h1>
         <div class="container">
             <?php foreach ($messages as $message) { ?>
-                <div class="row">
+                <div class="row" data-bs-toggle="modal" data-bs-target="#<?php echo $message['id'] ?>">
                     <h5>De : <?php echo htmlspecialchars($message["name"]); ?></h5>
-                    <h6><?php echo htmlspecialchars($message["email"]); ?></h6>
                     <h6>Sujet : <?php echo htmlspecialchars($message["subject"]); ?></h6><br>
-                    <p><?php echo htmlspecialchars($message["message"]); ?></p>
+                    <p class="message-overflow"><?php echo htmlspecialchars($message["message"]); ?></p>
+                </div>
+
+                <div class="modal fade" id="<?php echo $message['id'] ?>" tabindex="-1">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <div class="modal-title fs-5">
+                                    <h5>De : <?php echo htmlspecialchars($message["name"]); ?> (<?php echo htmlspecialchars($message["email"]); ?>)</h5>
+                                    <h6>Sujet : <?php echo htmlspecialchars($message["subject"]); ?></h6>
+                                </div>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fermer"></button>
+                            </div>
+                            <div class="modal-body">
+                                <p><?php echo htmlspecialchars($message["message"]); ?></p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+                                <button type="button" class="btn btn-danger" onclick="">Supprimer</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             <?php } ?>
         </div>
-
-        
-
         <?php include 'src/component/footer.php'; ?>
     </body>
 
