c6ac99753b3d6a0e21c5329b9c102768dc102715
KellyFemi <akpokelly1@gmail.com>
2025-03-20 11:55:47 +0100

    Deplacement des requetes sql

--- src/model/ArtModel/getArtModel.php
+++ src/model/ArtModel/getArtModel.php
@@ -5,7 +5,7 @@
 */
 require_once 'src/control/BDDControl/connectBDD.php';
 
-class UpdateArticleModel
+class GetArtModel
 {
 
     /*
@@ -16,5 +16,15 @@ class UpdateArticleModel
         $recupArt = $bdd->prepare('SELECT * FROM article WHERE id = ?');
         $recupArt->execute([$articleId]);
         return $recupArt->fetch(PDO::FETCH_ASSOC);
+        
     }
+
+    public function getFiltredArticles( PDO $bdd, $id, $title, $content, $user_id){
+        $query = $bdd->prepare("SELECT id, title, user_id, created_at, updated_at FROM article WHERE id LIKE ? AND title LIKE ? AND content LIKE ? AND user_id LIKE ? ORDER BY id LIMIT 50");
+        $query->execute(array($id, $title, $content, $user_id));
+        $articles = $query->fetchAll();
+        return $articles;
+    }
+
+
 }
