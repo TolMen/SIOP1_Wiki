8713ddaffedf4c5ea11e45ae2eaf6d77ccd2d8de
KellyFemi <akpokelly1@gmail.com>
2025-04-03 10:29:18 +0200

    Update Search.php

--- search.php
+++ search.php
@@ -40,53 +40,59 @@ if (!empty($_POST['mot_cle'])) {
     <?php include 'src/component/navbar.php'; ?>
     <div class="container">
         <h2>Résultat (s) pour "<?php echo htmlspecialchars($motCle); ?>"</h2>
-                </div>
-                <input class="recherche" type="submit" value="Rechercher" />
-            </form>
-        </div>
+    </div>
+    <div id="search_content">
+        <form class="formulaire" method="POST" action="search.php">
+            <div class="content_recherche">
+                <img src="assets/svg/search.svg" alt="">
+                <input type="search" name="mot_cle" id="" placeholder="Entrez un mot-clé" required>
+            </div>
+            <input class="recherche" type="submit" value="Rechercher" />
+        </form>
+    </div>
 
-        <div class="row" >
-             <div class="articles-grid">
-                <?php if (!empty($articlesbymotcle)) {
-                    foreach ($articlesbymotcle as $articlebymotcle) { 
-                        $artPostModel = new ArtPostModel();
-                        $imageData = $artPostModel->getArticleImage($bdd, $articlebymotcle['id']);
-                        $imageUrl = $imageData['url'] ?? 'assets/img/section1background.jpg'; //  Image par défaut
-                    
-                        ?>
-                         <div class="article-card">
-                            <div class="image_contenu">
-                                <img src="<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'article">
-                            </div>
-                            <div class="content">
-                                <h3><?php echo htmlspecialchars($articlebymotcle['title']); ?></h3>
-                                <span class="date">Publié le : 
-                                    <?php if(empty($articlebymotcle['updated_at'])) {
-                                        echo date("d/m/Y à H:i:s", strtotime($articlebymotcle['created_at']));
-                                    } else {
-                                        echo date("d/m/Y à H:i:s", strtotime($articlebymotcle['updated_at']));
-                                    } ?></span>
-                                <div class="article_choix">
-                                    <a href="templateArt.php?articleID=<?php echo $articlebymotcle['id']; ?>" class="read-more">
-                                        Continuer la lecture
-                                    </a>
-                                    <?php if (!empty($_SESSION["userID"]) && $_SESSION["userRole"] === "admin") { ?>
-                                        <a href="src/control/ArtControl/deleteArt.php?articleID=<?php echo $articlebymotcle['id']; ?>">
-                                            <img src="assets/svg/trash.svg" alt="Supprimer l'article">
-                                        </a>
-                                    <?php   } ?>
-                                </div>
-                                <a href="historique.php?articleID=<?php echo $articlebymotcle['id']; ?>" class="read-morehistorique">Voir l'historique</a>
-                            </div>
+    <div class="row">
+        <div class="articles-grid">
+            <?php if (!empty($articlesbymotcle)) {
+                foreach ($articlesbymotcle as $articlebymotcle) {
+                    $artPostModel = new ArtPostModel();
+                    $imageData = $artPostModel->getArticleImage($bdd, $articlebymotcle['id']);
+                    $imageUrl = $imageData['url'] ?? 'assets/img/section1background.jpg'; //  Image par défaut
+
+            ?>
+                    <div class="article-card">
+                        <div class="image_contenu">
+                            <img src="<?= htmlspecialchars($imageUrl) ?>" alt="Image de l'article">
                         </div>
-                    
-                <?php }
-                } else {
-                    echo "<p>Aucun article trouvé pour ce mot-clé.</p>";
-                } ?>
-            </div> 
+                        <div class="content">
+                            <h3><?php echo htmlspecialchars($articlebymotcle['title']); ?></h3>
+                            <span class="date">Publié le :
+                                <?php if (empty($articlebymotcle['updated_at'])) {
+                                    echo date("d/m/Y à H:i:s", strtotime($articlebymotcle['created_at']));
+                                } else {
+                                    echo date("d/m/Y à H:i:s", strtotime($articlebymotcle['updated_at']));
+                                } ?></span>
+                            <div class="article_choix">
+                                <a href="templateArt.php?articleID=<?php echo $articlebymotcle['id']; ?>" class="read-more">
+                                    Continuer la lecture
+                                </a>
+                                <?php if (!empty($_SESSION["userID"]) && $_SESSION["userRole"] === "admin") { ?>
+                                    <a href="src/control/ArtControl/deleteArt.php?articleID=<?php echo $articlebymotcle['id']; ?>">
+                                        <img src="assets/svg/trash.svg" alt="Supprimer l'article">
+                                    </a>
+                                <?php   } ?>
+                            </div>
+                            <a href="historique.php?articleID=<?php echo $articlebymotcle['id']; ?>" class="read-morehistorique">Voir l'historique</a>
+                        </div>
+                    </div>
 
+            <?php }
+            } else {
+                echo "<p>Aucun article trouvé pour ce mot-clé.</p>";
+            } ?>
         </div>
+
+    </div>
     </div>
 
     <?php include 'src/component/footer.php'; ?>
