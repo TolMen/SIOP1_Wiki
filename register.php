6454c4b8408bbf2c855a5ecad898525603876faa
nolansio <nolanarsac105@gmail.com>
2025-04-03 11:43:29 +0200

    Amélioration du style et de la structure des pages de connexion et d'inscription, ajout de nouvelles propriétés CSS pour une meilleure interface utilisateur

--- register.php
+++ register.php
@@ -9,47 +9,48 @@ include_once 'checkBanned.php';
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <!-- Inclusion des balise meta -->
-    <?php include 'src/component/head.php'; ?>
-    <link rel="stylesheet" href="css/authStyle.css" />
-    <title>Wiki - Inscription</title>
-</head>
+    <head>
+        <!-- Inclusion des balise meta -->
+        <?php include 'src/component/head.php'; ?>
+        <link rel="stylesheet" href="css/authStyle.css" />
+        <title>Wiki - Inscription</title>
+    </head>
 
-<body>
-    <!-- Inclusion de la barre de navigation -->
-    <?php include 'src/component/navbar.php' ?>
-    <div class="superContent" style="margin: 0; padding: 0; min-height: 82vh; display: flex; flex-direction: column;">
-        <div class="container">
-            <h1>S'inscrire</h1>
+    <body>
+        <!-- Inclusion de la barre de navigation -->
+        <?php include 'src/component/navbar.php' ?>
+        <div class="superContent"
+            style="margin: 0; padding: 0; min-height: 82vh; display: flex; flex-direction: column;">
+            <div class="container">
+                <h1>S'inscrire</h1>
+            </div>
+            <main class="container">
+                <form method="POST" action="src/control/UserControl/userregister.php">
+                    <label>Pseudonyme :</label><br>
+                    <input type="text" name="username" minlength="2" maxlength="15" pattern="[a-z0-9._]{2,15}"
+                        title="Seules les lettres minuscules, chiffres, '.' et '_' sont autorisés" required /><br>
+
+                    <label>Mot de passe :</label><br>
+                    <input type="password" name="password" required /><br><br>
+
+                    <?php if (!empty(htmlspecialchars(!empty($_GET["invalid"]), ENT_QUOTES))) { ?>
+                        <label class="invalidCase">Identifiant déjà existant</label><br><br>
+                    <?php } else { ?>
+                        <label></label><br>
+                    <?php } ?>
+                    <input type="submit" value="Inscription" />
+                    <div class="switchAuth">Déjà un compte ?&nbsp;<a href="login.php">Se connecter</a></div>
+                </form>
+            </main><br>
         </div>
-        <main class="container">
-            <form method="POST" action="src/control/UserControl/userregister.php">
-                <label>Pseudonyme :</label><br>
-                <input type="text" name="username" minlength="2" maxlength="15" pattern="[a-z0-9._]{2,15}"
-                    title="Seules les lettres minuscules, chiffres, '.' et '_' sont autorisés" required /><br>
 
-                <label>Mot de passe :</label><br>
-                <input type="password" name="password" required /><br><br>
+        <!-- Inclusion du pied de page -->
+        <?php include 'src/component/footer.php' ?>
 
-                <?php if (!empty(htmlspecialchars(!empty($_GET["invalid"]), ENT_QUOTES))) { ?>
-                    <label class="invalidCase">Identifiant déjà existant</label><br><br>
-                <?php } else { ?>
-                    <label></label><br>
-                <?php } ?>
-                <input type="submit" value="Inscription" />
-            </form>
-        </main><br>
-        <div class="switchAuth">Déjà un compte ?&nbsp;<a href="login.php">Se connecter</a></div>
-    </div>
-
-    <!-- Inclusion du pied de page -->
-    <?php include 'src/component/footer.php' ?>
-
-    <!-- Liens vers les scripts JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-</body>
+        <!-- Liens vers les scripts JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+            </script>
+    </body>
 
 </html>
--- src/model/UserModel/banCheckUserModel.php
+++ src/model/UserModel/banCheckUserModel.php
@@ -16,4 +16,6 @@ class banCheckUserModel {
         return $state->fetch();
     }
 
+    
+
 }
