c73d1d7068ac7b56e93b1ccc46cec1c0cce0df9f
nolansio <nolanarsac105@gmail.com>
2025-04-10 10:22:11 +0200

    Correction du fichier dashboard.php et resetAllArt.php.

--- src/control/ArtControl/resetAllArt.php
+++ src/control/ArtControl/resetAllArt.php
@@ -3,14 +3,11 @@
 session_name("main");
 session_start();
 include_once '../BDDControl/connectBDD.php'; // $bdd
-include_once '../../model/ArtModel/deleteArtModel.php';
 
 if (empty($_SESSION["userID"]) || $_SESSION["userRole"] != "admin") {
     header("Location: javascript://history.go(-1)");
     exit;
 }
-
-$deleteArtProcess = new DeleteArtModel();
 
 if (!empty($_SESSION["userID"])) {
     $articleID = htmlspecialchars($_GET["articleID"], ENT_QUOTES);
