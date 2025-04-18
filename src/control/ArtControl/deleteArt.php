6992f7702521290f917b8162ec8694a8d0278704
TolMen Jy <cotton.frachisse@gmail.com>
2025-04-18 17:13:25 +0200

    Redirection correct

--- src/control/ArtControl/deleteArt.php
+++ src/control/ArtControl/deleteArt.php
@@ -11,9 +11,16 @@ $articleID = htmlspecialchars($_GET["articleID"], ENT_QUOTES);
 $deleteArtProcess = new DeleteArtModel();
 $firstAuthorId = $deleteArtProcess->firstAuthorArt($bdd, $articleID);
 
-if ($_SESSION["userRole"] == "admin"  ||  $_SESSION["userID"] == $firstAuthorId["firstAuthor"]) {
+if ($_SESSION["userRole"] == "admin" || $_SESSION["userID"] == $firstAuthorId["firstAuthor"]) {
     $deleteArtProcess->deleteArt($bdd, $articleID);
 }
 
-header("Location: ../../../home.php");
+// Vérifie si la variable HTTP_REFERER est définie
+if (isset($_SERVER['HTTP_REFERER'])) {
+    // Redirige vers la page précédente
+    header("Location: " . $_SERVER['HTTP_REFERER']);
+} else {
+    // Si HTTP_REFERER n'est pas disponible, redirige vers la page d'accueil par défaut
+    header("Location: ../../../home.php");
+}
 exit;
